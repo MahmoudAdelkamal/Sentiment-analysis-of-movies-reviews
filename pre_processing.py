c71c7abda9dbaa3ef58206e39f241281f55ab071
malaa4085 <mahmoudadel2310@gmail.com>
2022-05-05 14:17:54 +0200

    Finished the preprocessing phase

---
+++ pre_processing.py
@@ -0,0 +1,86 @@
+import os
+import re
+import pandas as pd
+import nltk
+from sklearn.model_selection import KFold
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
+
+def read_text_file(file_path):
+
+    file = ""
+    with open(file_path, 'r') as f:
+        file+=f.read()
+    return file
+
+def read_files(path):
+
+    files = []
+    for file in os.listdir():
+        if file.endswith(".txt"):
+            file_path = f"{path}\{file}"
+            files.append(read_text_file(file_path))
+    return files
+
+def load_dataset() :
+
+    negative_dataset_path = "D:\\college material\\4th year\\2nd term\\NLP\\txt_sentoken\\neg"
+    positive_dataset_path = "D:\\college material\\4th year\\2nd term\\NLP\\txt_sentoken\\pos"
+    os.chdir(negative_dataset_path)
+    negative_dataset = read_files(negative_dataset_path)
+    os.chdir(positive_dataset_path)
+    positive_dataset = read_files(positive_dataset_path)
+
+    return positive_dataset,negative_dataset
+
+def remove_stop_words(dataset):
+
+    stop_words = set(stopwords.words("english"))
+    for i in range(len(dataset)):
+        dataset[i] = [word for word in dataset[i] if word not in stop_words]
+    return dataset
+
+def remove_punctuation(dataset):
+
+    for i in range(len(dataset)):
+        dataset[i] = re.sub("[^a-zA-z | ^\w+'t]",' ', dataset[i])
+        dataset[i] = word_tokenize(dataset[i])
+    return dataset
+
+def stemming(dataset):
+
+    porter_stemmer = PorterStemmer()
+    for i in range(len(dataset)):
+        dataset[i] = [porter_stemmer.stem(word) for word in dataset[i]]
+        dataset[i] = ' '.join(dataset[i])
+    return dataset
+
+def preprocess(dataset):
+
+    dataset = remove_punctuation(dataset)
+    dataset = remove_stop_words(dataset)
+    dataset = stemming(dataset)
+    return dataset
+
+positive_dataset, negative_dataset = load_dataset()
+preprocess(positive_dataset)
+positive_dataset = pd.DataFrame(positive_dataset)
+positive_dataset.columns = ['Review']
+positive_dataset['Label'] = ["pos" for i in range(1000)]
+
+preprocess(negative_dataset)
+negative_dataset = pd.DataFrame(negative_dataset)
+negative_dataset.columns = ['Review']
+negative_dataset['Label'] = ["neg" for i in range(1000)]
+
+dataset = pd.concat([positive_dataset,negative_dataset],ignore_index=True,sort=False)
+
+kf=KFold(n_splits=20,random_state=1,shuffle=True)
+for train_index,test_index in kf.split(dataset):
+    x = dataset['Review']
+    y = dataset['Label']
+    x_train , x_test, y_train , y_test= x[train_index],x[test_index],y[train_index],y[test_index]
+
+print(len(x_train),len(x_test))
